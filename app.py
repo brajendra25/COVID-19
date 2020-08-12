ca2476962fbc39da8f995d7d6d7f473840479754
Brajendra Kumar Shukla <brajendrashukla25@gmail.com>
2020-08-12 23:00:27 +0530

    Covid-19 starting page

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template,request
 import json 
-import Corona_Analysis as GA
-import Corona_Files_Download as DL
+import BusinessLayer.Corona_Analysis as GA
+import BusinessLayer.Corona_Files_Download as DL
 import pandas as pd
 import sys
 import feedparser
-import Covid19Data as CD
+import BusinessLayer.Covid19Data as CD
 import urllib.request
-from xml.etree.ElementTree import parse
+import world_report as WR
+import BusinessLayer.chatbotBL as CBL
+
 
 app = Flask(__name__)
+
 colors = [
     "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
     "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
@@ -30,34 +33,45 @@ def getStates():
 
 @app.route('/')
 def index():
-    try:
-        objc = GA.CoronaAnalysis()
-        df_State_Wise =  objc.Get_state_wise('TT')
-        #total_cases = str(sum(df_State_Wise[:1][action]))
-        df_State_Wise = df_State_Wise[1:]
-        df_filter = df_State_Wise.iloc[:,0:7]
-        '''
-        var_url = urllib.request.urlopen('https://search.bvsalud.org/global-literature-on-novel-coronavirus-2019-ncov/?output=rss&lang=en&from=0&sort=&format=summary&count=20&fb=&page=1&skfp=&index=tw&q=')
-        xmldoc = parse(var_url)
-        print(xmldoc)    '''
-        feed ="" # feedparser.parse('https://search.bvsalud.org/global-literature-on-novel-coronavirus-2019-ncov/?output=rss&lang=en&from=0&sort=&format=summary&count=20&fb=&page=1&skfp=&index=tw&q=')
-        return render_template('app.html',  tables=df_filter,feed=feed, titles=df_filter.columns.values)
-    except:
-        return render_template("error.html", error="Something else went wrong.")
+    action=""
+    action = request.args.get("action",default="Confirmed")
+    g_color = '#337ab7'
+    if action is None:
+        action = "Confirmed"
+    if action =='Recovered':
+        g_color = 'green'
+    if action =='Deaths':
+        g_color = 'red'
+    if action =='Active':
+        g_color = 'red'
+    
+    title = action
+    action = "Total" + action
 
-@app.route('/India')
-def india():
-    try:
-        objc = GA.CoronaAnalysis()
-        df_State_Wise =  objc.Get_state_wise('TT')
-        #total_cases = str(sum(df_State_Wise[:1][action]))
-        df_State_Wise = df_State_Wise[1:]
-        df_filter = df_State_Wise.iloc[:,0:7]
+    df_TotalRecords = WR.WorldTotal()
+    Total_Cases = list(df_TotalRecords.values())
+    active = int(Total_Cases[0]) - (int(Total_Cases[1]) + int(Total_Cases[2]))
+    Total_Cases.append(active)
 
-        feed = feedparser.parse('http://zeenews.india.com/hindi/india.xml')
-        return render_template('app.html',  tables=df_filter,feed=feed.entries, titles=df_filter.columns.values)
-    except:
-        return render_template("error.html", error="Something else went wrong.")
+    df_Countries = WR.GetCountries()
+    
+    df_Summary = WR.world_summary()
+    _notification = {}
+    _notification["New Confirmed"] = str(sum(df_Summary['NewConfirmed']))
+    _notification["New Recovered"] = str(sum(df_Summary['NewRecovered']))
+    _notification["New Deaths"] = str(sum(df_Summary['NewDeaths']))
+    #print(_notification)
+
+    #df_Country = WR.world_summary()
+    bar_labels = df_Summary["Country"][0:].head(30)
+    bar_values = df_Summary[action][0:].head(30)
+    
+    df_Summary = df_Summary.sort_values(action,ascending=False)
+
+    return render_template('index.html', title="Total " + title + " Cases in World",Total = Total_Cases,max=25000, 
+                            color=g_color,labels=bar_labels, values=bar_values, c_color=colors,
+                            contries = df_Countries, summary=df_Summary, notification = _notification)
+
 
 @app.route('/about')
 def about():
@@ -91,6 +105,7 @@ def download():
 def wordldb():
     try:
         objdownload = CD.Summary()
+        CD.WorldTotal()
         if objdownload == "1":
             files_download = "Download Process Completed Successfully!!"
             return render_template('download.html', title='About this Project', download = files_download)
@@ -108,15 +123,17 @@ def wordldb():
          error_msg = "Unexpected error:", sys.exc_info()[0]
          return render_template("error.html", error=error_msg)
 
+'''Optimized Code'''
 @app.route('/Country')
 def Country():
     try:
-        g_color = '#FFA833'
-        labels = df_Case_Time["Date"]
-        values = df_Case_Time["Daily Confirmed"]
-        bar_labels=labels
-        bar_values=values
-        
+       
+        objc = GA.CoronaAnalysis()
+        df_filter = pd.DataFrame()
+        name = request.args.get("name",default="India")
+        view = request.args.get("view",default="Graph")
+      
+        g_color = '#337ab7'
         Total_Confirmed = sum(df_Case_Time.tail(1)['Total Confirmed'])
         Total_Recovered = sum(df_Case_Time.tail(1)['Total Recovered'])
         Total_Deceased = sum(df_Case_Time.tail(1)['Total Deceased'])
@@ -124,16 +141,38 @@ def Country():
         
         total_cases = {"Total Confirmed":Total_Confirmed, "Total Active": Total_Active , "Total Recovered" : Total_Recovered , 
                        "Total Deceased" : Total_Deceased }
-        return render_template('country.html', title='Daily COVID-19 Cases in India', max=4500, cases_dict=total_cases, color=g_color, labels=bar_labels, values=bar_values)
+
+        if name == "India" and view == "Tbl":
+            df_State_Wise =  objc.Get_state_wise('TT')
+           
+            #total_cases = str(sum(df_State_Wise[:1][action]))
+            df_State_Wise = df_State_Wise[1:]
+            df_filter = df_State_Wise.iloc[:,0:8]
+            df_filter.sort_values(by=['Confirmed'], inplace=True,ascending=False)
+            row, columns = df_filter.shape
+            sr_no=[]
+            for index in range(row):
+                sr_no.append(index+1)
+            df_filter["S.No"] = sr_no
+            return render_template('app.html',title="COVID-19 Cases in India" ,tables=df_filter,
+            titles=df_filter.columns.values,cases_dict=total_cases)
+
+        if name == "India" and view == "Graph":
+            labels = df_Case_Time["Date"]
+            values = df_Case_Time["Daily Confirmed"]
+            bar_labels=labels
+            bar_values=values
+            return render_template('country.html', title='COVID-19 Cases in India', max=4500,
+                                cases_dict=total_cases, color=g_color, labels=bar_labels, values=bar_values)
     except:
         return render_template("error.html", error="Something else went wrong.")
 
-
+'''Optimized Code'''
 @app.route('/action')
 def action():
     try:
         g_max = 4500
-        g_color = '#FFA833'
+        g_color = '#337ab7'
        
         action = request.args.get("action")
         if action is None:
@@ -159,14 +198,14 @@ def action():
         
         total_cases = {"Total Confirmed":Total_Confirmed, "Total Active": Total_Active , "Total Recovered" : Total_Recovered , 
                        "Total Deceased" : Total_Deceased }
-        return render_template('country.html', title= action + ' Corona Growth', max=g_max, cases_dict=total_cases, color=g_color, labels=bar_labels, values=bar_values)
+        return render_template('country.html', title= action[6:] + ' Corona Growth in India', max=g_max, cases_dict=total_cases, color=g_color, labels=bar_labels, values=bar_values)
     except:
         return render_template("error.html", error="Something else went wrong.")
 
 @app.route('/dictrict')
 def dictrict():
     try:
-        g_color = '#FFA833'
+        g_color = '#337ab7'
         stateList = getStates()
         objc = GA.CoronaAnalysis()
         df_District =  objc.Get_District_By_State_Code('UP')
@@ -181,38 +220,19 @@ def dictrict():
     except:
         return render_template("error.html", error="Something else went wrong.")
 
-@app.route('/state')
-def state():
-    try:
-        
-        g_color = '#FFA833'
-        stateList = getStates()
-        objc = GA.CoronaAnalysis()
-        df_State_Wise =  objc.Get_state_wise('TT')
-        df_State_Wise = df_State_Wise[1:]
-        df_CoronaFree = df_State_Wise[df_State_Wise['Active'] == 0]
-        corona_free_state = list(df_CoronaFree['State'])
-        df_filter = df_State_Wise[df_State_Wise['Confirmed'] >= 5]
-       
-        bar_labels = df_filter['State']
-        bar_values = df_filter['Confirmed']
-        
-        return render_template('state.html', title='State Wise Confirmed Cases', max=25000, color=g_color, corona_Free=corona_free_state,
-                               labels=bar_labels, values=bar_values,states = stateList , Total_Cases={})
-    except:
-        return render_template("error.html", error="Something else went wrong.")
-    
-@app.route('/statebyaction/')
+'''optimized code''' 
+@app.route('/state/')
 def statebyaction():
     try:
-        g_color = '#FFA833'
+        g_color = '#337ab7'
         stateList = getStates()
         objc = GA.CoronaAnalysis()
-        #g_max = 5000
-        g_color = '#FFA833'
-        action = request.args.get("action")
-        state_code = request.args.get("code")
-        state = request.args.get("state")
+
+        state = request.args.get("state",default="Delhi")
+        state_code = request.args.get("code", default="DL")
+        view = request.args.get("view",default="Graph")
+        action = request.args.get("action",default="Confirmed")
+
         ''' Corona Free'''
         df_State_Wise =  objc.Get_state_wise('TT')
         df_State_Wise = df_State_Wise[1:]
@@ -237,7 +257,7 @@ def statebyaction():
          
         df_Confirmed = _df[_df['Status']== 'Confirmed']
         Total_Confirmed = sum(df_Confirmed[state_code])
-        print(Total_Confirmed)
+       
         df_Recovered = _df[_df['Status']== 'Recovered']
         Total_Recovered = sum(df_Recovered[state_code])
 
@@ -250,8 +270,8 @@ def statebyaction():
                        "Total Deceased" : Total_Deceased }
 
 
-        return render_template('state.html', title="Daily " + action +" cases in state " + state, max=g_max, color=g_color, 
-                               corona_Free=corona_free_state,labels=bar_labels, values=bar_values,states = stateList, Total_Cases=total_cases)
+        return render_template('state.html', title= action +" cases in state " + state, max=g_max, color=g_color, 
+                               corona_Free=corona_free_state,labels=bar_labels, values=bar_values,states = stateList, cases_dict=total_cases)
     except:
         return render_template("error.html", error="Something else went wrong.")
 
@@ -259,7 +279,7 @@ def statebyaction():
 def dictrictbystate():
     try:
         state_code = request.args.get("code")
-        print(state_code)
+       
         objc = GA.CoronaAnalysis()
         df_District =  objc.Get_District_By_State_Code(state_code)
         bar_labels = df_District['District']
@@ -267,16 +287,15 @@ def dictrictbystate():
     except:
         print("Something else went wrong.")
 
-
 @app.route('/dictrictbyaction')
 def dictrictbyaction():
     try:
-        g_color = '#FFA833'
+        g_color = '#337ab7'
         stateList = getStates()
         objc = GA.CoronaAnalysis()
         #g_max = 5000
         action=""
-        g_color = '#FFA833'
+        g_color = '#337ab7'
         state_code = request.args.get("code",default="AN")
         state = request.args.get("state",default="AN")
         if state_code is None:
@@ -302,67 +321,10 @@ def dictrictbyaction():
     except:
         return render_template("error.html", error="Something else went wrong.")
 
-@app.route('/confirmed')
-def confirmed():
-    try:
-        objc = GA.CoronaAnalysis()
-        df_State_Wise =  objc.Get_state_wise('TT')
-        #total_cases = str(sum(df_State_Wise[:1][action]))
-        df_State_Wise = df_State_Wise[1:]
-        labels = df_State_Wise['State']
-        values = df_State_Wise['Confirmed']
-        return render_template('state.html', title='State Wise Confirmed Cases', max=25000, sat=zip(values, labels, colors))
-    
-    except:
-        return render_template("error.html", error="Something else went wrong.")
-    
-@app.route('/world')
-def world():
-    import world_report as WR
-    try:
-        df_Country = WR.world_summary()
-        df_TotalRecords = WR.WorldTotal()
-        print(df_TotalRecords)
-        feed = "" #feedparser.parse('https://search.bvsalud.org/global-literature-on-novel-coronavirus-2019-ncov/?output=rss&lang=en&from=0&sort=&format=summary&count=20&fb=&page=1&skfp=&index=tw&q=')
-        return render_template('world.html',title="Corona Cases in World",feed=feed,
-        tables=df_Country,TotalRecords=df_TotalRecords)
-    
-    except OSError as err:
-        error_msg = "OS error: {0}".format(err)
-        return render_template("error.html", error=error_msg)
-    except EnvironmentError as eberr:
-        error_msg = "OS error: {0}".format(eberr)
-        return render_template("error.html", error=error_msg)
-    except:
-         error_msg = "Unexpected error from App page:", sys.exc_info()[0]
-         return render_template("error.html", error=error_msg)
 
-@app.route('/grahview')
-def world_grahview():
     import world_report as WR
     try:
-        g_color = '#FFA833'
-        df_Country = WR.world_summary()
-        bar_labels = df_Country["Country"].head(30)
-        bar_values = df_Country["TotalConfirmed"].head(30)
-        return render_template('world_graphview.html', title="Corona Cases in World" , max=25000, color=g_color,
-                               labels=bar_labels, values=bar_values)
-    
-    except OSError as err:
-        error_msg = "OS error: {0}".format(err)
-        return render_template("error.html", error=error_msg)
-    except EnvironmentError as eberr:
-        error_msg = "OS error: {0}".format(eberr)
-        return render_template("error.html", error=error_msg)
-    except:
-         error_msg = "Unexpected error from App page:", sys.exc_info()[0]
-         return render_template("error.html", error=error_msg)
-     
-@app.route('/wordl_action')
-def wordl_action():
-    import world_report as WR
-    try:
-        g_color = '#FFA833'
+        g_color = '#337ab7'
         action = request.args.get("action",default="TotalConfirmed")
         if action is None:
             action = "Confirmed"
@@ -377,7 +339,7 @@ def wordl_action():
         df_Byaction = WR.world_summary()
         bar_labels = df_Byaction["Country"].head(30)
         bar_values = df_Byaction[action].head(30)
-        return render_template('world_graphview.html', title='World Wise Cases', max=25000, color=g_color,
+        return render_template('index.html', title='World Wise Cases', max=25000, color=g_color,
                                labels=bar_labels, values=bar_values)
     
     except OSError as err:
@@ -389,6 +351,13 @@ def wordl_action():
     except:
          error_msg = "Unexpected error from App page:", sys.exc_info()[0]
          return render_template("error.html", error=error_msg)
+
+'''Chat Bot '''
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(CBL.chat_bow(userText.lower()))
+'''End'''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
